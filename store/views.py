b5131ba1397a2dec60930c81c0742da6686c8387
Sefa Sertkaya <sefqi52@gmail.com>
2023-06-20 19:55:04 +0300

    kart

--- store/views.py
+++ store/views.py
@@ -109,19 +109,12 @@ def home(request, category_slug=None):
     if category_slug !=None:
         categories     = get_object_or_404(Category, slug=category_slug)
         products       = Product.objects.filter(category=categories, is_available=True)
-        #filter
-        myFilter       = ProductFilter(request.GET, queryset=products)
-        products       = myFilter.qs
         paginator      = Paginator(products, 18)
         page           = request.GET.get("page")
         paged_products = paginator.get_page(page)
 
-
     else:
         products       = Product.objects.all().filter(is_available=True).order_by("-id")
-        #filter
-        myFilter       = ProductFilter(request.GET, queryset=products)
-        products       = myFilter.qs
         paginator      = Paginator(products, 18)
         page           = request.GET.get("page")
         paged_products = paginator.get_page(page)
@@ -135,7 +128,6 @@ def home(request, category_slug=None):
         "products"      : paged_products,
         "categories"    : categories,
         "setting"       : setting,
-        "myFilter"      : myFilter,
 #        'show_cookie_policy': show_cookie_policy
     }
 
@@ -157,6 +149,10 @@ def filter_results(request):
     #filter
     myFilter       = ProductFilter(request.GET, queryset=products)
     products       = myFilter.qs
+
+     # Fiyata göre sıralama
+    products = products.order_by('price')
+
     paginator      = Paginator(products, 33)
     page           = request.GET.get("page")
     try:
