edd6e74475bf44c6a1fb4035a871f48e47e685c2
Sefa Sertkaya <sefqi52@gmail.com>
2023-07-01 17:50:45 +0300

    ok

--- store/filters.py
+++ store/filters.py
@@ -3,8 +3,8 @@ import django_filters
 from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
-    price_gt = django_filters.NumberFilter(field_name="price", lookup_expr="gt")
-    price_lt = django_filters.NumberFilter(field_name="price", lookup_expr="lt")
+    price_gt = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    price_lt = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
 
     class Meta:
         model = Product
