66d8ddc6af7477af250a8c419509887c6a7c9587
Sefa Sertkaya <sefqi52@gmail.com>
2023-07-07 16:26:17 +0300

    kart

--- store/views.py
+++ store/views.py
@@ -115,13 +115,13 @@ def home(request, category_slug=None):
     if category_slug !=None:
         categories     = get_object_or_404(Category, slug=category_slug)
         products       = Product.objects.filter(category=categories, is_available=True)
-        paginator      = Paginator(products, 15)
+        paginator      = Paginator(products, 12)
         page           = request.GET.get("page")
         paged_products = paginator.get_page(page)
 
     else:
         products       = Product.objects.all().filter(is_available=True).order_by("-id")
-        paginator      = Paginator(products, 15)
+        paginator      = Paginator(products, 12)
         page           = request.GET.get("page")
         paged_products = paginator.get_page(page)
 
